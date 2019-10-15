347759b39a3a2362a656b94c9d08417d9add5177
stagiaire <stagiaire@PC-B-6-95R9J32>
2019-10-15 14:57:40 +0200

    Ajoutez des fichiers projet.

---
+++ Donnee.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWindDb
+{
+    class Donnee
+    {
+        SqlConnection conn = new SqlConnection();
+        SqlCommand cmd = new SqlCommand();
+        //SqlDataAdapter da = new SqlDataAdapter();
+        
+        // DataSet ds = new DataSet();
+
+
+        public void connecter()
+        {
+            if (conn.State == ConnectionState.Closed)
+            {
+                conn.ConnectionString = "Server=localhost\\sqlexpress01;Initial Catalog=NorthWind;Integrated Security=true";
+
+                conn.Open();
+            }
+        }
+
+        public void deconnecter()
+        {
+            if (conn.State == ConnectionState.Open)
+            {
+                conn.Close();
+            }
+        }
+
+        public SqlDataReader remplir(string table, string value)
+        {
+            this.connecter();
+            cmd.Connection = conn;
+            cmd.CommandText = "SELECT TOP 20 * FROM " + table+ " WHERE Country = @Country";
+          
+            cmd.Parameters.AddWithValue("@Country", value);
+            SqlDataReader reader = cmd.ExecuteReader();
+            return reader;
+            this.deconnecter();
+
+            
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NorthWindDb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*SqlConnection s = new SqlConnection("Server=localhost\\sqlexpress01;Initial Catalog=NorthWind;Integrated Security=true");
+            s.Open();
+
+            SqlCommand cmd = new SqlCommand("SELECT TOP 20 * FROM Customers WHERE Country = @Country", s);
+            cmd.Parameters.AddWithValue("@Country", "France");
+
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            while (reader.Read())
+            {
+                Console.WriteLine(reader.GetString(reader.GetOrdinal("Country")));
+            }*/
+
+            Donnee d = new Donnee();
+            SqlDataReader reader = d.remplir("Customers", "France");
+
+            while (reader.Read())
+            {
+                Console.WriteLine(reader.GetString(reader.GetOrdinal("ContactName")));
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
